550866fcea0af791c2a3b5b0c6293deb0b4da15c
Tanner Townsend <rsmcwow@gmail.com>
2020-04-09 00:12:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,28 +2,28 @@ require 'pry'
 
 class Artist
   attr_accessor :name, :songs
-  
+
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @songs = []
     save
   end
-  
+
   def save
     @@all << self
   end
-  
+
   def self.all
     binding.pry
     @@all
   end
-  
+
   def add_song(song)
    @songs << song
-  end  
-  
+  end
+
   def self.find_or_create_by_name(artist_name)
     found_artist = self.all.find {|artist| artist.name == artist_name}
     if found_artist
@@ -34,7 +34,7 @@ class Artist
       new_artist
     end
   end
-  
+
   def print_songs
     @songs.each { |song| puts song.name }
   end
